2c2de64f9a02d0e45e97388388d1cce09969519e
Giang Pham <giangpn25@gmail.com>
2026-02-21 14:18:41 -0600

    Part 1: multithread

--- concurrency-webserver/src/wclient.c
+++ concurrency-webserver/src/wclient.c
@@ -20,8 +20,10 @@
 //
 
 #include "io_helper.h"
+#include <pthread.h>
 
 #define MAXBUF (8192)
+#define NUMREQ 100
 
 //
 // Send an HTTP request for the specified file 
@@ -41,6 +43,8 @@ void client_send(int fd, char *filename) {
 //
 // Read the HTTP response and print it out
 //
+FILE* fp;
+
 void client_print(int fd) {
     char buf[MAXBUF];  
     int n;
@@ -48,7 +52,7 @@ void client_print(int fd) {
     // Read and display the HTTP Header 
     n = readline_or_die(fd, buf, MAXBUF);
     while (strcmp(buf, "\r\n") && (n > 0)) {
-	printf("Header: %s", buf);
+	fprintf(fp, "Header: %s", buf);
 	n = readline_or_die(fd, buf, MAXBUF);
 	
 	// If you want to look for certain HTTP tags... 
@@ -61,32 +65,77 @@ void client_print(int fd) {
     // Read and display the HTTP Body 
     n = readline_or_die(fd, buf, MAXBUF);
     while (n > 0) {
-	printf("%s", buf);
+	fprintf(fp, "%s", buf);
 	n = readline_or_die(fd, buf, MAXBUF);
     }
 }
 
-int main(int argc, char *argv[]) {
-    char *host, *filename;
+typedef struct {
+    char* host, *filename;
     int port;
-    int clientfd;
+} thread_args;
+
+
+void* threads_send(void* arg){
+    thread_args* args = (thread_args*)arg;
+    int clientfd = open_client_fd_or_die(args->host, args->port);
+    client_send(clientfd, args->filename);
+    client_print(clientfd);
+    close_or_die(clientfd);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     
     if (argc != 4) {
 	fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
 	exit(1);
     }
-    
-    host = argv[1];
-    port = atoi(argv[2]);
-    filename = argv[3];
+
+    fp = fopen("msg.txt", "w");
+
+    thread_args* args = malloc(sizeof(thread_args));
+    args->host = argv[1];
+    args->port = atoi(argv[2]);
+    args->filename = argv[3];
     
     /* Open a single connection to the specified host and port */
-    clientfd = open_client_fd_or_die(host, port);
+    // clientfd = open_client_fd_or_die(host, port);
     
-    client_send(clientfd, filename);
-    client_print(clientfd);
+    // client_send(clientfd, filename);
+    // client_print(clientfd);
     
-    close_or_die(clientfd);
-    
+    // close_or_die(clientfd);
+
+    pthread_t *threads = malloc(NUMREQ * sizeof(pthread_t));
+    if (!threads) {
+        perror("malloc\n");
+        exit(1);
+    }
+
+
+    for (int i = 0; i < NUMREQ; i++){
+        int rc = pthread_create(&threads[i], NULL, threads_send, (void *)args);
+        if (rc != 0){
+            fprintf(stderr, "pthread_create failed at %d\n", i);
+            for (int j = 0; j < i; j++){
+                pthread_join(threads[j], NULL);
+            }
+            free(threads);
+            free(args);
+            fclose(fp);
+            exit(1);
+        }
+    }
+    for (int i = 0; i < 100; i++){
+        int rc = pthread_join(threads[i], NULL);
+        if (rc != 0){
+            perror("pthread_join failed\n");
+
+        }
+    }
+    free(args);
+    fclose(fp);
+    free(threads);
     exit(0);
 }
--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include "request.h"
 #include "io_helper.h"
+#include <bits/getopt_core.h>
+#include <pthread.h>
+
+pthread_cond_t has_work = PTHREAD_COND_INITIALIZER; // same with using pthread_cond_init()
+pthread_cond_t available_buffer = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock_buffer = PTHREAD_MUTEX_INITIALIZER;
+
+int num_work = 0;
+int start = 0, end = 0;
+int buffer_size = 1;
+
 
 char default_root[] = ".";
+
+void* handling_request(void* arg){
+	int* buffer = (int*) arg;
+	while (1){
+		pthread_mutex_lock(&lock_buffer);
+
+		while (num_work == 0){
+			pthread_cond_wait(&has_work, &lock_buffer);
+		}
+
+		int connfd = buffer[start];
+		start = (start+1)%buffer_size;
+		num_work--;
+		pthread_cond_signal(&available_buffer);
+		pthread_mutex_unlock(&lock_buffer);
+
+		printf("Thread %x is handling request:\n", (unsigned long)pthread_self());
+		request_handle(connfd);
+		close_or_die(connfd);
+	}
+
+}
 
 //
 // ./wserver [-d <basedir>] [-p <portnum>] 
@@ -11,8 +44,10 @@ int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
+	int threads = 1;
+	char* alg = "FIFO"; // static data (not on heap or stack)
     
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
+    while ((c = getopt(argc, argv, "d:p:t:b:s:")) != -1)
 	switch (c) {
 	case 'd':
 	    root_dir = optarg;
@@ -20,23 +55,65 @@ int main(int argc, char *argv[]) {
 	case 'p':
 	    port = atoi(optarg);
 	    break;
+	case 't':
+		threads = atoi(optarg);
+		break;
+	case 'b':
+		buffer_size = atoi(optarg);
+		break;
+	case 's':
+		alg = optarg;
+		break;
 	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+	    fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffer] [-s algo]\n");
 	    exit(1);
 	}
 
     // run out of this directory
     chdir_or_die(root_dir);
 
+	// char cwd[256]; 
+    // if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    //     printf("Current working directory: %s\n", cwd);
+    // } else {
+    //     perror("getcwd error");
+    // }
+
+	// create buffers to store requests
+	int* buffer = malloc(buffer_size * sizeof(int));
+
+	// create pool of workers
+	pthread_t* workers = malloc(threads * sizeof(pthread_t));
+	for (int i = 0; i < threads; i++){
+		pthread_create(&workers[i], NULL, handling_request, buffer);
+	}
+
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
     while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
+		struct sockaddr_in client_addr;
+		int client_len = sizeof(client_addr);
+		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+		// request_handle(conn_fd);
+		// close_or_die(conn_fd);
+
+		// acquire the lock for the buffer
+		// put into the buffer
+
+		pthread_mutex_lock(&lock_buffer);
+		while (num_work == buffer_size) { // wait for the buffer has empty slots
+			pthread_cond_wait(&available_buffer, &lock_buffer);
+		}
+
+		buffer[end] = conn_fd;
+		num_work++;
+		end = (end+1)%buffer_size;
+		pthread_cond_signal(&has_work);
+		pthread_mutex_unlock(&lock_buffer);
     }
+	
+	free(buffer);
+	free(workers);
     return 0;
 }
 
